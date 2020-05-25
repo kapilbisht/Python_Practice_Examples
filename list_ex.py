b9730f57be430acf4b803a23cd2d7a2e06d144c5
Kapil Bisht <kapilbisht.1@gmail.com>
2020-05-25 10:38:42 +0530

    lists example

--- list_ex.py
+++ list_ex.py
@@ -21,3 +21,10 @@ list1 = ["Hello ", "take "]
 list2 = ["Dear", "Sir"]
 list3 = [i + j for i in list1 for j in list2]
 print(list3)
+
+# Iterate both lists simultaneously
+# such that list1 should display item in original order and list2 in reverse order.
+list1 = [1, 2, 3, 4, 5]
+list2 = [100, 200, 300, 400, 500]
+for x, y in zip(list1, list2[::-1]):
+    print(x, y)
